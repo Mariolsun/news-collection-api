953d2ced581904d4f7189800b3c78dccb5423132
mariolsun <mariolsun@yandex.ru>
2020-04-08 13:23:01 +0300

    добавлена валидация запроса на создание пользователя

--- routes/signup/signup.js
+++ routes/signup/signup.js
@@ -1,15 +1,7 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { isValidUserSchema } = require('../../middlewares/validation');
 const { createUser } = require('../controllers/createUser');
 
-router.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().uri(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), createUser);
+router.post('/', isValidUserSchema, createUser);
 
 module.exports = router;
