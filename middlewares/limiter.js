d42192998061ebbca17dd68b25256bd8eca939ff
mariolsun <mariolsun@yandex.ru>
2020-04-13 00:24:30 +0300

    bug fix rate limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit');
 const TooManyRequestsError = require('../errors/too-many-requests-err');
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 150,
+  windowMs: 5 * 60 * 1000,
+  max: 100,
   handler: (req, res, next) => {
     next(new TooManyRequestsError('Превышен лимит запросов. Попробуйте позже.'));
   },
