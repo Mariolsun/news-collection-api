573d5e77668031833724dc38174bef67047d548d
mariolsun <mariolsun@yandex.ru>
2020-04-13 00:03:05 +0300

    bug fix rate limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -3,9 +3,9 @@ const TooManyRequestsError = require('../errors/too-many-requests-err');
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 2,
+  max: 1,
   handler: (req, res, next) => {
-    next(new TooManyRequestsError(`Too many requests - your IP ${req.connection.remoteAddress} is being rate limited`));
+    next(new TooManyRequestsError('Превышен лимит запросов. Попробуйте позже.'));
   },
 });
 
