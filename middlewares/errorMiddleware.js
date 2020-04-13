2cfafda6389b684e0c77b26dfbab4c5f9b4e6e8c
mariolsun <mariolsun@yandex.ru>
2020-04-14 01:20:34 +0300

    Стандартные сообщения базы больше не отправляются клиенту

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -2,7 +2,8 @@
 const errorTexts = require('../constants/errors');
 
 module.exports = (err, req, res, next) => {
-  const { statusCode = 500, message = errorTexts.INTERNAL_ERROR } = err;
+  const { statusCode = 500 } = err;
+  const message = statusCode === 500 ? errorTexts.INTERNAL_ERROR : err.message;
   if (res.headersSent) {
     return next(err);
   }
