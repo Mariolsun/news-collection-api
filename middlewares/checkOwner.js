9708f40eea8bae6e803f7546f6c57b4ffc10732f
mariolsun <mariolsun@yandex.ru>
2020-04-13 13:23:13 +0300

    исправлена ошибка удаления карточки

--- middlewares/checkOwner.js
+++ middlewares/checkOwner.js
@@ -3,11 +3,12 @@ const ForbiddenError = require('../errors/forbidden-err');
 const { MESSAGES } = require('../config');
 
 module.exports.checkOwner = (req, res, next) => {
-  Article.findById(req.params.id, (err, article) => {
-    if (!article.owner.equals(req.user._id)) {
-      next(new ForbiddenError(MESSAGES.ARTICLE_NOT_DELETED_NOT_OWNER));
-    }
-    next();
-  })
+  Article.findById(req.params.id).select('+owner')
+    .then((article) => {
+      if (!article.owner.equals(req.user._id)) {
+        throw new ForbiddenError(MESSAGES.ARTICLE_NOT_DELETED_NOT_OWNER);
+      }
+      next();
+    })
     .catch(next);
 };
