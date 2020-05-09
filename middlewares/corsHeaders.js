38efa72d46ee109134bc8e4f2f85ba20b6416fd0
mariolsun <mariolsun@yandex.ru>
2020-05-09 16:41:13 +0300

    cors: any

--- middlewares/corsHeaders.js
+++ middlewares/corsHeaders.js
@@ -2,7 +2,8 @@ const { ALLOWED_CORS } = require('../config');
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
-  if (ALLOWED_CORS.includes(origin)) {
+  // if (ALLOWED_CORS.includes(origin)) {
+  if (true) {
     res.header('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Credentials', 'true');
