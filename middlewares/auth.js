9b770cd1e32928349f5cf28af3eef1ed00bde30e
mariolsun <mariolsun@yandex.ru>
2020-05-09 23:24:19 +0300

    prefligth request bug fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,22 +5,23 @@ const { SECRET_STRING } = require('../config');
 const errorTexts = require('../constants/errors');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  let token;
-  if (req.cookies.jwt) {
-    token = req.cookies.jwt;
-  } else if (authorization && authorization.startsWith('Bearer ')) {
-    token = authorization.replace('Bearer ', '');
+  if (req.method !== 'OPTIONS') {
+    const { authorization } = req.headers;
+    let token;
+    if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    } else if (authorization && authorization.startsWith('Bearer ')) {
+      token = authorization.replace('Bearer ', '');
+    }
+
+    let payload;
+    try {
+      payload = jwt.verify(token, SECRET_STRING);
+    } catch (err) {
+      next(new NeedAuthError(errorTexts.AUTH_NEEDED));
+    }
+
+    req.user = payload;
   }
-
-  let payload;
-  try {
-    payload = jwt.verify(token, SECRET_STRING);
-  } catch (err) {
-    next(new NeedAuthError(errorTexts.AUTH_NEEDED));
-  }
-
-  req.user = payload;
-
   next();
 };
